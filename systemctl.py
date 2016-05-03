b402e10cb71c68af97bca302d4e250f338e36089
YokiToki <tamat@myttk.ru>
2016-05-03 13:48:43 +0600

    Add service description, add reload and start/stop button

--- systemctl.py
+++ systemctl.py
@@ -8,11 +8,20 @@ class Systemctl():
 	def run(self, pwd, action, service):
 		os.system('echo \'%s\'|sudo -S systemctl %s %s.service' % (pwd, action, service))
 
+	def is_enabled(self, service):
+		out = os.popen("systemctl is-enabled %s.service" % (service,)).read().strip()
+		if out == 'enabled':
+			return True
+		return False
+
 	def is_active(self, service):
 		out = os.popen("systemctl is-active %s.service" % (service,)).read().strip()
 		if out == 'active':
 			return True
 		return False
+
+	def description(self, service):
+		return os.popen("systemctl show -p Description %s.service" % (service,)).read().replace("Description=", "").strip()
 
 	def chk_pwd(self, pwd):
 		username = os.getenv('USER')
